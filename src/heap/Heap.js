3b94c83f22257492ca74a915d7b935e6efe2ec0a
zjhch123 <416836508@qq.com>
2018-06-21 16:06:35 +0800

    heap ok

--- src/heap/Heap.js
+++ src/heap/Heap.js
@@ -71,13 +71,104 @@ export default class Heap {
     return item
   }
 
-  
+  add(item) {
+    this.heapContainer.push(item)
+    this.heapifyUp()
 
-
-
-
-  heapifyDown() {
-    // TODO
+    return this
   }
 
+  remove(item, customFindingComparator) {
+    const compare = customFindingComparator || this.compare
+    const numberOfItemsToRemove = this.find(item, compare).length
+
+    for(let iteration = 0; iteration < numberOfItemsToRemove; iteration ++) {
+      const indexToMove = this.find(item, compare).pop()
+      if (indexToMove === this.heapContainer.length - 1) {
+        this.heapContainer.pop()
+      } else {
+        this.heapContainer[indexToMove] = this.heapContainer.pop()
+
+        const parentItem = this.hasParent(indexToMove) ? this.parent(indexToMove) : null
+        const leftChild = this.hasLeftChild(indexToMove) ? this.leftChild(indexToMove) : null
+
+        if (
+          leftChild !== null &&
+          (
+            parentItem === null ||
+            this.compare.lessThan(parentItem, this.heapContainer[indexToMove])
+          )
+        ) {
+          this.heapifyDown(indexToMove)
+        } else {
+          this.heapifyUp(indexToMove)
+        }
+      }
+    }
+    return this
+  }
+
+  find(item, customComparator) {
+    const foundItemIndices = []
+    const compare = customComparator || this.compare
+
+    for (let itemIndex = 0; itemIndex < this.heapContainer.length; itemIndex ++) {
+      if (compare.equal(this.heapContainer[itemIndex], item)) {
+        foundItemIndices.push(itemIndex)
+      }
+    }
+
+    return foundItemIndices
+  }
+
+  heapifyUp(customStartIndex) {
+    let currentIndex = customStartIndex || this.heapContainer.length - 1;
+
+    while (
+      this.hasParent(currentIndex) &&
+      this.compare.lessThan(this.heapContainer[currentIndex], this.parent(currentIndex))
+    ) {
+      this.swap(currentIndex, this.getParentIndex(currentIndex));
+      currentIndex = this.getParentIndex(currentIndex);
+    }
+  }
+
+  heapifyDown(customStartIndex) {
+    let currentIndex = customStartIndex || 0
+    let nextIndex = null
+
+    while(this.hasLeftChild(currentIndex)) {
+      if (
+        this.hasRightChild(currentIndex) && 
+        this.compare.lessThan(this.rightChild(currentIndex), this.leftChild(currentIndex))
+      ) {
+        nextIndex = this.getRightChildIndex(currentIndex)
+      } else {
+        nextIndex = this.getLeftChildIndex(currentIndex)
+      }
+
+      if (this.compare.lessThan(this.heapContainer[currentIndex], this.heapContainer[nextIndex])) {
+        break
+      }
+
+      this.swap(currentIndex, nextIndex)
+      currentIndex = nextIndex
+    }
+  }
+
+  isEmpty() {
+    return !this.heapContainer.length;
+  }
+
+  toString() {
+    return this.heapContainer.map(item => item.toString()).join(',')   
+  }
 }
+
+// for(let i = 0; i < 10; i++) {
+//   const a = new Heap()
+//   for(let j = 0; j < 10; j++) {
+//     a.add(Math.floor(Math.random() * 10))
+//   }
+//   console.log(a.toString())
+// }
