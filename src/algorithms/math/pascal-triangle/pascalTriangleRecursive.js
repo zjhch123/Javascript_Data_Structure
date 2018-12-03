e3e67e70c97d279b31ca5d031dafd3ab4b497c05
zjhch123 <416836508@qq.com>
2018-12-03 20:15:32 +0800

    feat: little complexNumber

--- src/algorithms/math/pascal-triangle/pascalTriangleRecursive.js
+++ src/algorithms/math/pascal-triangle/pascalTriangleRecursive.js
@@ -1,8 +1,21 @@
-export default function (lineNumber) {
+export default function pascalTriangle(lineNumber) {
   if (lineNumber === 0) {
     return [1]
   }
 
   const currentLineSize = lineNumber + 1
   const previousLineSize = currentLineSize - 1
+
+  const currentLine = []
+
+  const previousLine = pascalTriangle(lineNumber - 1)
+
+  for (let numIndex = 0; numIndex < currentLineSize; numIndex += 1) {
+    const left = (numIndex - 1) >= 0 ? previousLine[numIndex - 1] : 0
+    const right = numIndex < previousLineSize ? previousLine[numIndex] : 0
+
+    currentLine.push(left + right)
+  }
+
+  return currentLine
 }
